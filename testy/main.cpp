33de2b9cfc6561df7eaaf214c7838861568f86f2
majkipll <majkipll123@majkipll.laptop.l>
2023-10-17 09:32:37 +0200

    okno startowe

---
+++ testy/main.cpp
@@ -0,0 +1,38 @@
+    // SDL2 Hello, World!
+// This should display a white screen for 2 seconds
+// compile with: clang++ main.cpp -o Mini_Golf_Mobile -lSDL2
+// run with: ./Mini_Golf_Mobile
+#include <SDL2/SDL.h>
+#include <stdio.h>
+
+
+#define SCREEN_WIDTH 640
+#define SCREEN_HEIGHT 480
+
+
+
+int main(int argc, char* args[]) {
+  SDL_Window* window = NULL;
+  SDL_Surface* screenSurface = NULL;
+  if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+    fprintf(stderr, "could not initialize sdl2: %s\n", SDL_GetError());
+    return 1;
+  }
+  window = SDL_CreateWindow(
+			    "Mini Golf Mobile ",
+			    SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
+			    SCREEN_WIDTH, SCREEN_HEIGHT,
+			    SDL_WINDOW_SHOWN
+			    );
+  if (window == NULL) {
+    fprintf(stderr, "could not create window: %s\n", SDL_GetError());
+    return 1;
+  }
+  screenSurface = SDL_GetWindowSurface(window);
+  SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0, 0xFF, 0));
+  SDL_UpdateWindowSurface(window);
+  SDL_Delay(2000);
+  SDL_DestroyWindow(window);
+  SDL_Quit();
+  return 0;
+}
