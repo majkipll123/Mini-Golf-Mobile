16568bf9f70db94df0913d8c0148d0901fd17f32
majkipll <majkipll123@majkipll.laptop.l>
2023-10-29 13:17:13 +0100

    usuwanie zbednych plikow

--- testy/main.cpp
+++ testy/main.cpp
@@ -1,9 +1,13 @@
 #include <SDL2/SDL.h>
-#include <stdio.h>
-#include "Ball.cpp"
+#include <SDL2/SDL2_gfxPrimitives.h>
+#include <SDL2/SDL_ttf.h>
+#include <cmath>
+#include <iostream>
+#include "Ball.h" 
+const float FRICTION = 0.95;
 
-#define SCREEN_WIDTH 1080
-#define SCREEN_HEIGHT 940
+#define SCREEN_WIDTH 500
+#define SCREEN_HEIGHT 900
 
 int main(int argc, char* args[]) {
     SDL_Window* window = NULL;
@@ -14,6 +18,8 @@ int main(int argc, char* args[]) {
         return 1;
     }
 
+
+   
     window = SDL_CreateWindow("Mini Golf Mobile", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
         SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
     if (window == NULL) {
@@ -28,24 +34,70 @@ int main(int argc, char* args[]) {
     }
 
     Ball ball(20, SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2);
-
+    bool isDragging = false;
+    float prevMouseX = 0;
+    float prevMouseY = 0;
+    float xAcceleration = 0.0;
+    float yAcceleration = 0.0;
+    float deltaX = 0.0;
+    float deltaY = 0.0;
+    float mouseX = 0.0;
+    float mouseY = 0.0;
+    bool ready = true;
     bool quit = false;
     while (!quit) {
         SDL_Event e;
         while (SDL_PollEvent(&e) != 0) {
             if (e.type == SDL_QUIT) {
                 quit = true;
-            }
-        }
+            } // tutaj zablokowac uzytkownikowi mozliwosc uderzania kilka razy z rzedu
+            else if (e.type == SDL_MOUSEBUTTONDOWN) {
+                if (e.button.button == SDL_BUTTON_LEFT) {
+                    isDragging = true;
 
-        ball.move();
+                    prevMouseX = e.motion.x;
+                    prevMouseY = e.motion.y;
+                   
+                    
+                }
+
+            }
+            else if (e.type == SDL_MOUSEBUTTONUP && ball.isready()) {
+                if (e.button.button == SDL_BUTTON_LEFT) {
+                    isDragging = false;
+                    mouseX = e.motion.x;
+                    mouseY = e.motion.y;
+                    deltaX = mouseX - prevMouseX;
+                    deltaY = mouseY - prevMouseY;
+                    std::cout<<deltaX<<" deltaX \n";
+                    std::cout<<deltaY<<" deltaY \n";
+                    float xVelocity = static_cast<float>(deltaX) / SCREEN_WIDTH;
+                    float yVelocity = static_cast<float>(deltaY) / SCREEN_HEIGHT;
+                    ball.setAcceleration(xVelocity, yVelocity, 0.0);
+                    ball.setVelocity(xVelocity, yVelocity, 0.0);
+                    
+                    //std::cout<<deltaX<<" deltaX \n";
+                    //std::cout<<deltaY<<" deltaY \n";   
+                    //std::cout<<xVelocity<<" xaccel \n";
+                    //std::cout<<yVelocity<<" yaccel \n";
+                }
+            }
+            //else if (e.type ){
+
+                
+            //}
+           
+            
+        }
         ball.handleCollision(SCREEN_WIDTH, SCREEN_HEIGHT);
+        ball.move();
+        
 
         SDL_SetRenderDrawColor(renderer, 0, 0xFF, 0, 0xFF);
         SDL_RenderClear(renderer);
 
         ball.draw(renderer);
-
+        
         SDL_RenderPresent(renderer);
     }
 
